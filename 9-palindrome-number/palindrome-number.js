b64bdf0d29b39397f74c1252a65d404261f14a29
Chryste Perri <122880294+chrysteperri@users.noreply.github.com>
2026-03-23 18:35:10 +0300

    Time: 4 ms (89.89%) | Memory: 64 MB (58.37%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+function isPalindrome(x) {
+    const original = x;
+    let reversed = 0;
+
+    while (x > 0) {
+        let digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x = Math.floor(x / 10);
+    }
+
+    return original === reversed;
+}
